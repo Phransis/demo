70084da65e92cd2bb38132bbb8166b3e02cb3bfa
Duo Francis <duofrancis11@gmil.com>
2024-11-02 10:09:11 +0000

    Structs, Enums and Hashmaps

--- src/main.rs
+++ src/main.rs
@@ -275,16 +275,373 @@
 //                 }
            
            
-// Ownnership
-    fn main(){
-        let mut s = String::from("hello ");
-        s.push_str("handla");
-        println!("{s}");
-        println!("{}", s);
-        let s1 = String::from("hello");
-        println!("{}, Cassandra", s1);
-        let s2 = s1;
+// // Ownnership
+//     fn main(){
+//         let mut s = String::from("hello ");
+//         s.push_str("handla");
+//         println!("{s}");
+//         println!("{}", s);
+//         let s1 = String::from("hello");
+//         println!("{}, Cassandra", s1);
+//         let s2 = s1;
     
-        println!(" {}, world!", s2);
+//         println!(" {}, world!", s2);
     
+//     }
+
+
+
+// Slice
+// fn main() {
+//     let arr = [1, 2, 3, 4, 5];
+
+//     let slice1 = &arr[1..4];
+//     let slice2 = &arr[2..];
+//     let slice3 = &arr[..3];
+//     let slice4 = &arr[..];
+
+//     println!("slice1: {:?}", slice1);
+//     println!("slice2: {:?}", slice2);
+//     println!("slice3: {:?}", slice3);
+//     println!("slice4: {:?}", slice4);
+    
+//     let result: i32 = sum(&arr[1..5]);
+//     println!("Result: {result}")
+// }
+// fn sum(slice: &[i32]) -> i32{
+//     slice.iter().sum()
+// }
+
+
+
+
+
+
+// Stucts
+// fn main(){
+//     #[derive(Debug)]
+//     struct User{
+//         id: u128,
+//         firstname: String,
+//         lastname: String,
+//         email: String,
+//         active: bool,
+//     }
+//     let  user1 = User{
+//         id: 1,
+//         firstname: String::from("Handla"),
+//         lastname: String::from("Duo"),
+//         email:String::from("test@mail.com"),
+//         active: true,
+//     };
+//     println!("{user1:?}");
+
+    
+   
+   
+    // update_user(Stacy, Brown, bstacymailcom);
+//     fn update_user(firstname: String, lastname:String, email: String) -> User{
+//         User{
+//             id: 2,
+//             firstname,
+//             lastname,
+//             email,
+//             active: false
+//         }
+
+//     }
+
+//    let update = update_user("Stacy".to_string(), "Brown".to_string(), "bstacymailcom".to_string());
+//     println!("Update: {update:?}");
+
+
+    
+    
+    //Creating instances from other instances
+    // let user2 = User{
+    //     id: 3,
+    //     firstname: user1.firstname,
+    //     lastname: user1.lastname,
+    //     email: String::from("mail@test.com"),
+    //     active: true
+    // };
+    // println!("{user2:?}");
+
+  
+  
+   
+   //Using struct update syntax
+    // let user3 = User{
+    //     email: String::from("new@mail.com"),
+    //     ..user2
+    // };
+    // println!("{user3:?}");
+
+
+   
+   
+    //Tuple struct
+    // #[derive(Debug)]
+    // struct Color(i128, i128, i128);
+    // struct Point(i128, i128, i128);
+    
+    // let black=Color (0, 0, 0);
+    // let origin= Point(0, 0, 0);
+
+   
+   
+   //Example
+//    #[derive(Debug)]
+//     struct Rectangle{
+//          length: i128,
+//         breadth: i128,
+//     }
+    // area_rectangle(10, 5);
+//     let rect1 = Rectangle{
+//         length: 30,
+//         breadth: 15
+//     };
+//     println!("RECT1{:#?}",rect1 );
+//     println!("The area is {:#?}",area_rectangle(100, 50) )
+    
+// }
+// fn area_rectangle(lenght: i128, breadth: i128) -> i128{
+//     lenght * breadth
+// }
+
+
+// struct Rectangle{
+//     height: u32,
+//     width: u32,
+// }
+
+// struct Circle{
+//     pi: f32,
+//     radius: f32
+// }
+
+// impl Circle {
+//     fn area_circle(&self) -> f32{
+//         self.pi * (self.radius*self.radius)
+//     }
+// }
+
+// impl Rectangle {
+//     fn area_rectangle(&self) -> u32 {
+//         self.height * self.width
+//     }
+// }
+
+
+// fn main(){
+//     let rect1 = Rectangle{
+//         height : 50,
+//         width : 100,
+//     };
+
+//     println!("The area of a Rectangle with height {} and width {} is {}", rect1.height, rect1.width, rect1.area_rectangle());
+
+//     let cir1 = Circle{
+//         pi: 3.142,
+//         radius: 33.3,
+//     };
+//     println!("The area of a Circle with pi {} and radius {} is {}", cir1.pi, cir1.radius, cir1.area_circle())
+// }
+
+
+// use std::io;
+// fn main(){
+//     changes("Handla".to_string(), "Duo".to_string());
+
+//     fn changes(name:String, lname:String) {
+//         println!("What's your name?");
+
+//         let  name = String::new();
+//         let  lname= String::new();
+
+//         println!("the names are {} {}", name, lname)
+//     }
+// }
+
+
+//ENUMS
+
+// fn main() {
+//     enum Coin {
+//         Penny,
+//         Nickel,
+//         Dime,
+//         Quater,
+//     }
+
+//     fn value_in_cents(coin: Coin) -> u8 {
+//         match coin {
+//             Coin::Penny => 1,
+//             Coin::Nickel => 5,
+//             Coin::Dime => 10,
+//             Coin::Quater => 25,
+//         }
+//     }
+// }
+
+
+//MATCH AND ENUMS
+
+// fn main() {
+
+
+//     fn plus_one(x: Option<i32>) -> Option<i32> {
+//         match x {
+//             None => None,
+//             Some(i) => Some(i +1),
+//         }
+//     }
+
+//     let five = Some(5);
+//     let six = plus_one(five);
+//     let none = plus_one(None);
+
+//     println!("The Some function: {:?}", plus_one(six));
+//     println!("The None function: {:?}", plus_one(none));
+    
+// }
+
+
+// VECTORS
+// fn main() {
+
+//     let v1 = vec![100];
+//     println!("Integer Vector: {:?}", v1);
+
+//     let v2 = vec!["Hello Handla"];
+//     println!("String Vector: {:?}", v2);
+    
+//     let mut vector = Vec::new();
+//     vector.push(4);
+//     vector.push(6);
+//     vector.push(7);
+
+//     println!("Mutabale vector: {:?}", vector);
+
+//     let mut v = vec![1, 2, 3];
+//     v.push(3);
+//     v.push(2);
+//     v.push(1);
+//     let ve = vec![4,5,6];
+//     println!("Vector v: {:?}", v);
+
+
+    //Adding and Removing elements in a Vector
+//     let mut vector = vec![v, ve];
+    
+//     vector.push([10,20,30].to_vec());
+//     println!("Vector-Push: {:?}", vector);
+    
+//     vector.pop();
+//     println!("Vector-Pop: {:?}", vector);
+
+// }
+
+
+//HASHMAPS
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut scores = HashMap::new();
+
+//     scores.insert(String::from("Blue"), 10);
+//     scores.insert(String::from("Yellow"), 50);
+
+//     for (key, value) in &scores{
+//         println!("{key} {value}");
+//     }
+
+//     let team_name = String::from("Blue");
+//     let score = &team_name;
+
+//     println!("Score is {:?}", score);
+// }
+
+#![no_std]
+use soroban_sdk::{contract,contracttype, contractimpl, Env, Symbol,symbol_short, log};
+
+#[contracttype]
+#[derive(Clone)]
+pub struct Poll{
+    pub yes: u64,
+    pub no: u64,
+    pub total: u64,
+}
+
+const POLL: Symbol = symbol_short!("POLL");
+
+#[contracttype]
+pub enum Registry{
+    Record(Symbol)
+}
+
+#[contracttype]
+#[derive(Clone)]
+pub struct Record{
+    pub name: Symbol,
+    pub selected: Symbol,
+    pub votes: u64,
+    pub time: u64,
+}
+
+const YES: Symbol = symbol_short!("yes");
+// const No: Symbol = symbol_short!("no");
+
+
+#[contract]
+pub struct VoteContract;
+
+#[contractimpl]
+impl VoteContract {
+
+    pub fn register_votes(env: Env, user: Symbol, votes: u64, selected: Symbol) {
+        let mut records = Self::view_voter(env.clone(), user.clone());
+        let time = env.ledger().timestamp();
+
+        if votes ==0 || records.time != 0 || votes > 5 {
+            panic!("cannot vote");
+        }
+        else {
+            let mut poll = Self::view_poll(env.clone());
+            records.name = user.clone();
+            records.selected = selected;
+            records.votes = votes;
+            records.time = time;
+
+            if records.selected == YES {
+                poll.yes += votes;
+            }
+            else {
+                poll.no += votes;
+            }
+            poll.total += votes;
+
+            env.storage().persistent().set(&Registry::Record(user), &records);
+            env.storage().persistent().set(&POLL, &poll);
+            log!(&env, "Votes Registered");
+            return;
+        }
     }
+
+    pub fn view_poll(env:Env) -> Poll {
+        env.storage().instance().get(&POLL).unwrap_or(Poll{
+            yes: 0,
+            no: 0,
+            total: 0,
+        })
+    }
+    pub fn view_voter(env:Env, voter: Symbol) -> Record {
+        let key = Registry::Record(voter.clone());
+        env.storage().instance().get(&key).unwrap_or(Record{
+             name: symbol_short!("not_found"),
+             selected: symbol_short!("none"),
+             votes: 0,
+             time: 0,
+        })
+    }
+}
